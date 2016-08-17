d3b256781ce1638b2d760db973b168fb3c342631
atgerritsen <alida@uidaho.edu>
2016-08-16 17:18:35 -0700

    changed metadata format, added metadata to premium

--- postAbundancePremium.R
+++ postAbundancePremium.R
@@ -20,37 +20,17 @@ pAprop = t(t(pAmat)/colSums(pAmat))
 rare.idx = rowSums(pAprop > .01) > (ncol(pAprop) * .01)
 
 # read in metadata groupings
-FecalMeta.2014 <- read.csv("./testData/FecalMeta.2014.csv", header=T, sep='\t')
-FecalMeta.2014$Taxon_Name <- sub("^", "X", FecalMeta.2014$Taxon_Name)
-foodTreatment <- FecalMeta.2014$Treatment
-tissueOrigin <- FecalMeta.2014$Tissue
-
-# metadata = data.frame(samples=colnames(pAmat),
-#                       group1=sample(c('a','b','c'), size=ncol(pAmat), replace=T),
-#                       group2=sample(c(1,2,3), size=ncol(pAmat), replace=T),
-#                       group3=sample(c('tissue1','tissue2','tissue3','tissue4','tissue5'), size=ncol(pAmat), replace=T)
-#                       )
-
-
-#---Come up with unique colors per species/classification level.
-
-#-Create subsets for level of classification.
-
-# Might be more useful to 
-# Figure 2)
-#     Stacked barplot for community composition
-#     -using colors for species
-
-# Figure 3)
-#     Stacked barplot for subset of common species
-
-# Figure N)
+metadata <- read.csv("./testData/FecalMeta.2014.csv", header=T, sep=',')
+metadata$Sample_Name <- sub("^", "X", metadata$Sample_Name)
+foodTreatment <- metadata$Treatment
+tissueOrigin <- metadata$Tissue
+legs <- c(as.character(unique(foodTreatment)), as.character(unique(tissueOrigin)))
+# Figure 1)
 # hclust for community membership based on unfiltered percentages
-mdcolors = data.frame(colorRampPalette(c('blue','red'))(length(unique(metadata$group1)))[metadata$group1],
-                 colorRampPalette(c('green','yellow'))(length(unique(metadata$group1)))[metadata$group2],
-                 colorRampPalette(c('purple','pink'))(length(unique(metadata$group1)))[metadata$group3])
+mdcolors = data.frame(colorRampPalette(c('blue','red'))(length(unique(foodTreatment)))[foodTreatment],
+                 colorRampPalette(c("green", "orange"))(length(unique(foodTreatment)))[tissueOrigin])
 
-colnames(mdcolors) = colnames(metadata)[2:4]
+colnames(mdcolors) = colnames(metadata)[2:3]
 
 
 pdf(file="Hierarchical_clustering_proportions_wards_euclidean.pdf", width=8 + .1*ncol(pA), height=8)
@@ -60,10 +40,11 @@ pdf(file="Hierarchical_clustering_proportions_wards_euclidean.pdf", width=8 + .1
     plot(hc, main=paste("Hierarchical clustering using Ward's agglomeration and euclidean distances with proportional values"),
         xlab='', ylab='')
     par(mar=c(1,6,0, .5))
+    legend('topright', legend=legs, col=c("blue", "red", "green", "yellow"), pch=20, cex=1.5, pt.cex = 2, bty="n")
     plotColorUnderTree(hc, colors=mdcolors)
 dev.off()
 
-# Figure N+1)  MDS-scaling 2d plot of clusters colored by Grouping Factor 1, with pcr controlled by grouping factor 2
+# Figure 1.1)  MDS-scaling 2d plot of clusters colored by Grouping Factor 1, with pcr controlled by grouping factor 2
 loc = cmdscale(dist(t(pAprop)), k=3)
 x = loc[,1]
 y = loc[,2]
